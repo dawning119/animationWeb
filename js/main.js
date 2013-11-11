4f311ea1d5f260044f58b8498563e2eaecbc38f6
dawning119 <danwing119@gmail.com>
2013-11-11 22:41:09 +0800

    door effect updating fix some bugs

--- js/main.js
+++ js/main.js
@@ -1,15 +1,31 @@
+var	i = 0;								//   Global variable:
+var	animationId;						//	 Using for all the animation
+var	animationArray = new Array();		//	 automatically start and end.
+
 $(document).ready(function(){
 
-	$('header#branding').mouseenter(function(){
-		$(this).animate({opacity:"1.0"},'fast');
-		$(this).animate({height:"50px"},'fast');
-	})
-	.mouseleave(function(){
-		$(this).animate({opacity:"0.3"},'fast');
-		$(this).animate({height:"40px"},'fast');
-	});
+	detectPage();
+	detectPage();
 
+	//springAnimate();
+	//----------Bind the nav events---------------
+    $('nav#primary').localScroll({
+        onAfter:detectPage
+    });
+    
+    //----------Branding events-----------------
+	$('header#branding').hover(
+		function(){
+			$(this).animate({opacity:"1.0"},'fast');
+			$(this).animate({height:"50px"},'fast');
+		},
+		function(){
+			$(this).animate({opacity:"0.3"},'fast');
+			$(this).animate({height:"40px"},'fast');
+		}
+	);
 
+	//----------Navagation events-----------------
 	$('nav#primary a').hover(
 		function () {
 			$(this).prev('h1').css('visibility','visible');
@@ -19,65 +35,139 @@ $(document).ready(function(){
 		}
 	);
 
-	$('nav#primary a').click(function(){
-		var index = $(this).parent().index();
-		detectPage(index);
-	});
+	
 
-	var rotateInterval;
-	$(".leaf1").mouseenter(function(){
-		var angle = 0;
-		var self=$(this);
-		rotateInterval=setInterval(function(){
-      		angle+=10;
-    		self.rotate(angle);
-		},20);
 
-	})
-	.mouseleave(function(){
-		$(this).rotate({
-            duration:3000,
-            angle: 0, 
-            animateTo:360
-        })
-		clearInterval(rotateInterval);
-	});
-	var grassAngle=0;
+});
+
+function detectPage(){
+			stopAnimation();
+        	var currentScroll = $(window).scrollTop();
+			var spring_top = $('#spring').offset().top;
+			var summer_top = $('#summer').offset().top;
+			var autumn_top = $('#autumn').offset().top;
+			var winter_top = $('#winter').offset().top;
+			var soil_top = $('#soil').offset().top;
+
+			if(currentScroll<summer_top) {
+				springAnimate();
+			}
+			else if(currentScroll>=summer_top&&currentScroll<autumn_top){
+				summerAnimate();
+			}
+			else if(currentScroll>=autumn_top&&currentScroll<winter_top){ 
+				autumnAnimate();
+			}
+			else if(currentScroll>=winter_top&&currentScroll<soil_top){
+				winterAnimate();
+			}
+			else if(currentScroll>=soil_top){
+				soilAnimate();
+			}
+}
+	
+var grassAngle=0;
 	var orient=true;
-	setInterval(function(){
+	function grassEffect(){
       	if(orient==true){
       		
-      		grassAngle+=0.5;
+      		grassAngle+=0.4;
       		$(".grass").rotate({angle:grassAngle, center: ["50%", "100%"]});
-      		if(grassAngle==10)
+      		if(grassAngle>10)
       			orient=false;
       	}
       	else{
-      		grassAngle-=0.5;
+      		grassAngle-=0.2;
       		$(".grass").rotate({angle:grassAngle, center: ["50%", "100%"]});
-      		if(grassAngle==0)
+      		if(grassAngle<0)
       			orient=true;
       	}
-    },50);
-		
+	}
+
+
+//----------------Spring page--------------------------
+function springAnimate(){ 
+
+	animationId = setInterval(grassEffect,50);
 	
 	
-});
+
+    animationArray[i]=animationId;
+    i++;
+
+    // rotating leaf animation
+    var rotateInterval;
+	$(".leaf1").hover(
+		function(){
+			var angle = 0;
+			var self=$(this);
+			rotateInterval=setInterval(function(){
+	      		angle+=10;
+	    		self.rotate(angle);
+			},20);
+
+	},
+		function(){
+			clearInterval(rotateInterval);
+			$(this).rotate({
+	            duration:3000,
+	            angle: 0, 
+	            animateTo:360
+	        })
+	});
+
+	//door effect
+	$("#door").hover(
+		function(){
+			$('#door_left').animate({left:"-142px"},'slow');
+			$('#door_right').animate({right:"-142px"},'slow');
+		},
+		function(){
+			$('#door_left').animate({left:"0px"},'slow');
+			$('#door_right').animate({right:"2px"},'slow');
+		}
+	);
 
 
-function detectPage(index){
 
+}	
+//-------------------------Summer page--------------------------
+function summerAnimate(){
+	animationId = setInterval(grassEffect2,50);
+	animationArray[i]=animationId;
+    i++;
+	var grassAngle=0;
+	var orient=true;
+	function grassEffect2(){
+      	if(orient==true){
+      		
+      		grassAngle+=0.4;
+      		$(".grasstest").rotate({angle:grassAngle, center: ["50%", "100%"]});
+      		if(grassAngle>10)
+      			orient=false;
+      	}
+      	else{
+      		grassAngle-=0.2;
+      		$(".grasstest").rotate({angle:grassAngle, center: ["50%", "100%"]});
+      		if(grassAngle<0)
+      			orient=true;
+      	}
+	}
+}
+//-------------------------Autumn page--------------------------
 
+function autumnAnimate(){ }
+//-------------------------Winter page--------------------------
+function winterAnimate(){ }
+//-------------------------Soil page--------------------------
+function soilAnimate(){ }
 
-	//var currentScroll = $(window).scrollTop();
-	//var homepage_top = $('#homepage').offset().top;
-	//var history_top = $('#history').offset().top;
-	//var interest_top = $('#interest').offset().top;
-	//var lookingFor_top = $('#lookingFor').offset().top;
-	//alert (currentScroll);
-	//if(index==0) 
-	//	$('nav#primary').fadeOut('slow');
-	//else
-	//	$('nav#primary').fadeIn();
+//Stop all the animation when changing the page.
+function stopAnimation(){
+
+		for(; i > 0; i--){
+  			clearInterval(animationArray[i-1]);                      
+    	}                       
+	
 
 }
